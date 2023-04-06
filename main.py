0acd4cc06a0fc9e35fbe2cfaf34d77e5393e0b75
Bruno Cruz <brunocruzz@gmail.com>
2023-04-06 15:09:40 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import paramiko
+import getpass
+# specify the host information
+host = ""
+port = 22
+username = ""
+passwd = getpass.getpass("Password: ")
+
+ssh = paramiko.SSHClient()
+print(f"Trying to --> ssh {username}@{host}:{port}")
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=host, username=username, password=passwd,port=port)
+
+stdin, stdout, stderr = ssh.exec_command('ls -l')
+output = stdout.readlines()
+
+# print the output of the command
+print(output)
+
+
+#Ends the connection
+ssh.close()
